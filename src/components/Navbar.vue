ee07a69a68f112e851199609395f9a9bbc811bff
TillConvertible <till.muehlenberend@htwg-konstanz.de>
2026-06-23 21:02:56 +0200

    Alle Links in Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,21 +5,43 @@ const baseUrl = import.meta.env.BASE_URL
 </script>
 
 <template>
-    <nav class="navbar navbar-custom sticky-top py-1">
+    <nav class="navbar navbar-expand-lg navbar-light navbar-custom sticky-top py-1">
         <div class="container">
             <router-link class="navbar-brand" to="/">
                 <img :src="`${baseUrl}internship-finder-logo.png`" alt="Internship Finder Logo" height="64">
             </router-link>
 
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <router-link class="nav-link" to="/">Praktika</router-link>
-                </li>
-            </ul>
+            <div class="d-flex align-items-center order-lg-last">
+                <UserMenu class="me-2 me-lg-0" />
+                <button class="navbar-toggler ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
 
-            <!-- Profil-Dropdown - immer sichtbar -->
-            <div class="ms-auto">
-                <UserMenu />
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-4">
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/">Praktika</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/meine-praktika">Meine Praktika</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/bewerbungen">Bewerbungen</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/nutzer">Nutzerverwaltung</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/datenschutz">Datenschutz</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/impressum">Impressum</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/kontakt">Kontakt</router-link>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
